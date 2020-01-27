f684d83e00ce73759308c6d106e87990d04749a5
mohammadrezamousaviniya <m.mousavi.ct.z.mrm@gmail.com>
2020-01-27 00:39:53 -0800

    global.asax.cs changed

--- PCMSP_MVC/Global.asax.cs
+++ PCMSP_MVC/Global.asax.cs
@@ -13,9 +13,6 @@ namespace PCMSP_MVC
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
 
