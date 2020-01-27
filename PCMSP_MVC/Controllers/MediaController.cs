fe0dc983bdb14ef181428f7bc1bc24ac7f9a077d
user <user@DESKTOP-BERDRJR>
2020-01-27 14:10:28 +0330

    Email

--- PCMSP_MVC/Controllers/MediaController.cs
+++ PCMSP_MVC/Controllers/MediaController.cs
@@ -4,36 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace PCMSP_MVC.Controllers
 {
-    public class MediaController : ApiController
+    public class MediaController : Controller
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        public ActionResult Email()
         {
-            return new string[] { "value1", "value2" };
+            return Content("Email");
         }
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
     }
 }
